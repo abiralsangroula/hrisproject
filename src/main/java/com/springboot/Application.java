9a6e925f193c035c9eba5af637c91637b244674b
abiral777 <contact.abiral@gmail.com>
2020-02-06 17:57:28 +0545

    web config

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springboot.model.User;
 import com.springboot.repository.UserRespository;
 
 @SpringBootApplication
+@Controller
 public class Application implements CommandLineRunner {
 	@Autowired
 	private UserRespository userRepository;
@@ -31,13 +36,13 @@ public class Application implements CommandLineRunner {
 			user.setIsAdmin(true);
 			user.setCreatedDate(new Date());
 			user.setDelFlg(false);
-			userRepository.save(user);	
-			Optional<User> findUser= userRepository.findById(user.getId());
-			if(findUser.isPresent()) {
-				User existingUser= findUser.get();
+			userRepository.save(user);
+			Optional<User> findUser = userRepository.findById(user.getId());
+			if (findUser.isPresent()) {
+				User existingUser = findUser.get();
 				existingUser.setCreatedBy(user);
 			}
-				
 		}
 	}
+
 }
